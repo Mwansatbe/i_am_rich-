79f008b3a0061fa2b27083dcdb659f7fd6aad6e7
Mwansa Chilekwa <mwansachille@gmail.com>
2023-01-04 19:15:26 +0200

    Intial Commit

---
+++ android/app/src/main/kotlin/mwansachile/com/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package mwansachile.com.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//Main function is the entry point for program execution
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Center(
+        child: Text("Hello World"),
+      ),
+    ),
+  );
+}
